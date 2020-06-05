2b81e4c213294b8fc30558f97a80e4fb62a3a8d4
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-06-04 23:46:53 -0400

    Add enigma test it exists

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -1,2 +1,10 @@
-require './test_helper'
-require './lib/enigma'
+require_relative './test_helper'
+require_relative './lib/enigma'
+
+class EnigmaTest < MiniTest::Test
+  def test_it_exists
+    enigma = Enigma.new
+
+    assert_instance_of Enigma, enigma
+  end
+end
