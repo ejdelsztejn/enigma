8f8e07983931c637755bf8aa8791aedef5f6237e
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-06-05 00:02:44 -0400

    Add initial test method names to match to outlined method concepts

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -7,4 +7,28 @@ class EnigmaTest < MiniTest::Test
 
     assert_instance_of Enigma, enigma
   end
+
+  def test_it_can_generate_a_key
+    skip
+  end
+
+  def test_it_can_return_if_there_is_a_date
+    skip
+  end
+
+  def test_it_can_generate_todays_date
+    skip
+  end
+
+  def test_it_can_generate_offsets
+    skip
+  end
+
+  def test_it_can_generate_shifts
+    skip
+  end
+
+  def test_it_can_encrypt
+    skip
+  end
 end
