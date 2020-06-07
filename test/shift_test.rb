cba1b562425ad95a056bb425c9d25260ead22e1d
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-06-07 13:00:39 -0400

    Add test it has attributes

--- test/shift_test.rb
+++ test/shift_test.rb
@@ -8,4 +8,11 @@ class ShiftTest < MiniTest::Test
 
     assert_instance_of Shift, shift
   end
+
+  def test_it_has_attributes
+    shift = Shift.new
+
+    assert_equal Time.now.strftime("%d%m%y"), shift.date
+    assert_equal 5, shift.key.length
+  end
 end
