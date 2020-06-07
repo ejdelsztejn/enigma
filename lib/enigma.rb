f43d7d3df0dfcfc9c41cc05686f859dbd8f3f908
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-06-07 11:09:25 -0400

    Add generate_offsets(date = generate_date)

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -21,5 +21,13 @@ class Enigma
     Time.now.strftime("%d%m%y")
   end
 
-  
+  def generate_offsets(date = generate_date)
+    offset_date = (date.to_i * date.to_i).to_s[2, 4]
+    {
+      A: offset_date[0].to_i,
+      B: offset_date[1].to_i,
+      C: offset_date[2].to_i,
+      D: offset_date[3].to_i
+    }
+  end
 end
