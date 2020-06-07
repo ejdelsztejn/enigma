ac1c8db0f7eb915e90eba5b61c2dd11c56494a49
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-06-07 10:11:46 -0400

    Add test it has characters

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -8,22 +8,28 @@ class EnigmaTest < MiniTest::Test
     assert_instance_of Enigma, enigma
   end
 
+  def test_it_has_characters
+    enigma = Enigma.new
+
+    assert_instance_of Array, enigma.characters 
+  end
+
   def test_it_can_generate_keys
     skip
     enigma = Enigma.new
 
     p enigma.generate_keys
     # assert_equal 5, key.length
+  end
 
-    def test_it_can_return_if_there_is_a_date
-      skip
-    end
+  def test_it_can_return_if_there_is_a_date
+    skip
+  end
 
-    def test_it_can_generate_todays_date
-      skip
-      enigma = Enigma.new
+  def test_it_can_generate_todays_date
+    skip
+    enigma = Enigma.new
 
-      p enigma.generate_date
-    end
+    p enigma.generate_date
   end
 end
