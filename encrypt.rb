d75f8de986513910246390bb658c6606931c0e69
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-06-07 15:35:24 -0400

    Add ability to encrypt message

--- encrypt.rb
+++ encrypt.rb
@@ -1,5 +1,17 @@
+require './lib/enigma'
+require './lib/shift'
+
+enigma = Enigma.new
+shift = Shift.new
+
 handle = File.open(ARGV[0], "r")
 to_encrypt = handle.read
 handle.close
 
-puts to_encrypt
+encrypted = enigma.encrypt(to_encrypt, shift.key, shift.date)[:encryption]
+
+writer = File.open(ARGV[1], "w")
+writer.write(encrypted)
+writer.close
+
+puts "Created '#{ARGV[1]}' with the key #{shift.key} and date #{shift.date}"
