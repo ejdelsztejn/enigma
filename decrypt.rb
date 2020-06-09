c41dd3cf6bb2a0c3247e51be2b68a1c9047f6cf9
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-06-09 10:12:23 -0400

    decrypt.rb hot fix

--- decrypt.rb
+++ decrypt.rb
@@ -7,7 +7,7 @@ handle = File.open(ARGV[0], "r")
 to_decrypt = handle.read
 handle.close
 
-decrypted = enigma.decrypt(to_decrypt, ARGV[2], ARGV[3])[:encryption]
+decrypted = enigma.decrypt(to_decrypt, ARGV[2], ARGV[3])[:decryption]
 
 writer = File.open(ARGV[1], "w")
 writer.write(decrypted)
